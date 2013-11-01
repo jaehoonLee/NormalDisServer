f14acf85a34e2e40fda1ae9b21e9acf8f3a0b4ff
lee jaehoon <jaehoonlee88@gmail.com>
2013-11-01 15:57:48 +0900

    add git ignore serveral changes in html files

--- Main/views.py
+++ Main/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import *
 
 # Create your views here.
 def main(request):
-    return render_to_response('index.html', RequestContext(request))
+    loop = [1, 2, 3]
+    return render_to_response('test.html', RequestContext(request, {'loops' : loop}))
+    #return render_to_response('index.html', RequestContext(request, {'loops' : loop}))
